10ea5554418ef7090b5a82dbf7d38f7d733e3984
Reyjerges <jesalcedoreyes@gmail.com>
2026-02-23 21:36:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def adia_cerebro(mensaje, historial):
     try:
         # Usamos Llama 3.1 8B para evitar bloqueos por Rate Limit
         completion = groq_client.chat.completions.create(
-              model="qwen-2.5-coder-32b",
+              model="llama-3.3-70b-specdec",
             messages=mensajes_ia,
             temperature=0.4
         )
